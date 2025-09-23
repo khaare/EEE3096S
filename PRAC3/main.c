d3909b199aa9ee1be1b3da43dcb72ee545df34a3
khaare <166560462+khaare@users.noreply.github.com>
2025-09-23 12:47:51 +0200

    main.c for F0. Code is the same for F4.
    code is the same for F0 and F4 except for #define TILE_W 256  #define TILE_H 256 and #include "stm32f4xx_it.h". These lines of code are for the F4

---
+++ PRAC3/main.c
@@ -0,0 +1,338 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include "stm32f0xx_it.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+#define MAX_ITER 100
+#define FIXED_POINT_SCALE 1000000LL  // 10^6 scale factor
+#define ESCAPE_RADIUS_SQUARED (4LL * FIXED_POINT_SCALE)   // scaled 4.0
+#define TILE_W 128  // adjust for F0 vs F4
+#define TILE_H 128
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+//TODO: Define and initialise the global varibales required
+/*
+  start_time
+  end_time
+  execution_time 
+  checksum: should be uint64_t
+  initial width and height maybe or you might opt for an array??
+*/
+uint32_t start_time = 0;       // Start time in ticks
+uint32_t end_time = 0;         // End time in ticks
+uint32_t execution_time = 0;   // Execution time in ticks (end_time - start_time)
+uint64_t checksum = 0;         // 64-bit checksum returned from mandelbrot function
+
+// Image dimensions for testing - square images of dimensions (128, 160, 192, 224, 256)
+const uint32_t IMAGE_DIMENSIONS[] = {128, 160, 192, 224, 256};
+const uint32_t NUM_DIMENSIONS = 5;
+
+// Current test dimensions - change these to test different sizes
+uint32_t width = 256;          // Current image width
+uint32_t height = 256;         // Current image height
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+uint64_t calculate_mandelbrot_fixed_point_arithmetic(int x_start, int y_start,
+                                         int tile_w, int tile_h,
+                                         int full_w, int full_h,
+                                         int max_iterations);
+uint64_t calculate_mandelbrot_double(int width, int height, int max_iterations);
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  //TODO: Turn on LED 0 to signify the start of the operation
+  /* USER CODE BEGIN 2 */
+// Turn on LED0 to signify start
+HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+
+// Record start time
+start_time = HAL_GetTick();
+
+// Determine if tiling is necessary
+uint32_t estimated_ram_needed = width * height; // bytes (1 byte/pixel)
+uint32_t available_ram = 128 * 1024; // STM32F4 SRAM in bytes, adjust for STM32F0
+// Compute Mandelbrot with tiles
+checksum = 0;
+for (int y0 = 0; y0 < height; y0 += TILE_H) {
+    for (int x0 = 0; x0 < width; x0 += TILE_W) {
+        checksum += calculate_mandelbrot_fixed_point_arithmetic(
+            x0, y0, TILE_W, TILE_H, width, height, MAX_ITER
+        );
+    }
+}
+
+// Record end time
+end_time = HAL_GetTick();
+execution_time = end_time - start_time;
+
+// Turn on LED1 to signify end
+HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+
+// Hold LEDs for 1 second
+HAL_Delay(1000);
+
+// Turn off LEDs
+HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0 | GPIO_PIN_1, GPIO_PIN_RESET);
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PB0 PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+uint64_t calculate_mandelbrot_fixed_point_arithmetic(int x_start, int y_start,
+                                         int tile_w, int tile_h,
+                                         int full_w, int full_h,
+                                         int max_iterations)
+{
+    uint64_t checksum = 0;
+    for (int y = 0; y < tile_h; y++) {
+        for (int x = 0; x < tile_w; x++) {
+            int global_x = x_start + x;
+            int global_y = y_start + y;
+
+            if (global_x >= full_w || global_y >= full_h) continue;
+
+            int64_t x0 = ((int64_t)global_x * (int64_t)(3.5 * FIXED_POINT_SCALE)) / full_w
+                         - (int64_t)(2.5 * FIXED_POINT_SCALE);
+            int64_t y0 = ((int64_t)global_y * (int64_t)(2.0 * FIXED_POINT_SCALE)) / full_h
+                         - FIXED_POINT_SCALE;
+
+            int64_t xi = 0, yi = 0;
+            int iteration = 0;
+
+            while (iteration < max_iterations) {
+                int64_t xi2 = (xi * xi) / FIXED_POINT_SCALE;
+                int64_t yi2 = (yi * yi) / FIXED_POINT_SCALE;
+                if (xi2 + yi2 > ESCAPE_RADIUS_SQUARED) break;
+
+                int64_t old_xi = xi;
+                xi = xi2 - yi2 + x0;
+                yi = (2 * old_xi * yi) / FIXED_POINT_SCALE + y0;
+
+                iteration++;
+            }
+
+            checksum += iteration;
+        }
+    }
+    return checksum;
+}
+
+uint64_t calculate_mandelbrot_double(int width, int height, int max_iterations) {
+    uint64_t checksum = 0;
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            double x0 = ((double)x / width) * 3.5 - 2.5;
+            double y0 = ((double)y / height) * 2.0 - 1.0;
+
+            double xi = 0.0;
+            double yi = 0.0;
+            int iteration = 0;
+
+            while (iteration < max_iterations) {
+                double xi2 = xi * xi;
+                double yi2 = yi * yi;
+
+                if (xi2 + yi2 > 4.0) {
+                    break;
+                }
+
+                double temp = xi2 - yi2 + x0;  // new xi
+                yi = 2.0 * xi * yi + y0;       // new yi (uses old xi)
+                xi = temp;                     // update xi
+
+                iteration++;
+            }
+
+            checksum += iteration;
+        }
+    }
+
+    return checksum;
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
